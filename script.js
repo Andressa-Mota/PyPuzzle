1c8167f79a6bd37e96a16fc30a9bf099bc752fc8
Andressa <dhee.andressa@gmail.com>
2024-08-01 22:52:18 -0300

    adcionando mascote

--- script.js
+++ script.js
@@ -37,3 +37,29 @@ function dragStart(event) {
     function dragOver(event) {
     event.preventDefault();
   }
+  function getRandomPosition(element) {
+    const slider = document.querySelector('.slider');
+    const sliderWidth = slider.clientWidth;
+    const sliderHeight = slider.clientHeight;
+    const elementWidth = element.clientWidth;
+    const elementHeight = element.clientHeight;
+
+    const randomX = Math.floor(Math.random() * (sliderWidth - elementWidth));
+    const randomY = Math.floor(Math.random() * (sliderHeight - elementHeight));
+
+    return { top: randomY, left: randomX };
+}
+
+function moveImageRandomly() {
+    const image = document.querySelector('.python');
+    const newPosition = getRandomPosition(image);
+
+    image.style.top = `${newPosition.top}px`;
+    image.style.left = `${newPosition.left}px`;
+}
+
+// Move the image to a random position every 2 seconds
+setInterval(moveImageRandomly, 4000);
+
+// Initial random position
+moveImageRandomly();
